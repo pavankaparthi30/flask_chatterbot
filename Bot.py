33d8c9bece33f535b62fbd415b3e4e1368bbc82f
pavan <kumarkpavan11@gmail.com>
2019-03-21 14:03:24 +0530

    initial commit

---
+++ Bot.py
@@ -0,0 +1,51 @@
+from flask import Flask, render_template, request
+
+from chatterbot import ChatBot
+
+from chatterbot.trainers import ListTrainer
+import os
+
+
+
+app = Flask(__name__)
+
+
+
+mvsr_bot = ChatBot("Chatterbot", storage_adapter="chatterbot.storage.SQLStorageAdapter")
+
+logic_adapters=[
+	
+        
+       {
+            'import_path': 'chatterbot.logic.LowConfidenceAdapter',
+            'threshold': 0.65,
+            'default_response': "I'm currently unable to answer that. Please try being more specific."
+        }
+    ],
+
+
+
+
+@app.route("/")
+
+def home():
+
+    return render_template("index.html")
+
+
+
+@app.route("/get")
+
+def get_bot_response():
+
+    userText = request.args.get('msg')
+
+    return str(mvsr_bot.get_response(userText))
+
+
+
+
+
+if __name__ == "__main__":
+
+    app.run()
